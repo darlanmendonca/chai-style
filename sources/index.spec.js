3148eacfbd6fd5f7200b4bd235c2341539a06b98
Darlan Mendonça <darlanmendonca@gmail.com>
2017-05-03 17:51:22 -0300

    separate specs by module, properties and value

--- sources/index.spec.js
+++ sources/index.spec.js
@@ -10,27 +10,41 @@ beforeEach(function createElement() {
 })
 
 describe('chai-style', () => {
-  it('should exports a named function', () => {
-    const module = require('./index.js')
-    expect(module).to.be.a('function')
-    expect(module.name).to.be.equal('chaiStyle')
-  })
-
-  it('should assert success', () => {
-    expect(element).to.have.style('color', 'red')
-  })
-
-  it('should assert fail', () => {
-    expect(() => {
-      expect(element).to.have.style('color', 'blue')
+  describe('module', () => {
+    it('should exports a named function', () => {
+      const module = require('./index.js')
+      expect(module).to.be.a('function')
+      expect(module.name).to.be.equal('chaiStyle')
     })
-    .to.throw('expect DIV to have a blue color, is receiving red')
   })
 
-  it('should assert fail, using .not', () => {
-    expect(() => {
-      expect(element).to.not.have.style('color', 'red')
+  describe('properties', () => {
+    it('should assert success', () => {
+      expect(element).to.have.style('color')
     })
-    .to.throw('expect DIV to not have a red color, is receiving red')
+
+    it('should assert success', () => {
+      expect(element).to.not.have.style('background-color')
+    })
+  })
+
+  describe('values', () => {
+    it('should assert success', () => {
+      expect(element).to.have.style('color', 'red')
+    })
+
+    it('should assert fail', () => {
+      expect(() => {
+        expect(element).to.have.style('color', 'blue')
+      })
+      .to.throw('expect DIV to have a blue color, is receiving red')
+    })
+
+    it('should assert fail, using .not', () => {
+      expect(() => {
+        expect(element).to.not.have.style('color', 'red')
+      })
+      .to.throw('expect DIV to not have a red color, is receiving red')
+    })
   })
 })
