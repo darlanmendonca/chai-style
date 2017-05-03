4c18f26f6665228ea5722a6d401fdc02fb47a8d2
Darlan Mendonça <darlanmendonca@gmail.com>
2017-05-03 15:34:07 -0300

    add spec: should exports a named function

---
+++ sources/index.js
@@ -0,0 +1,8 @@
+module.exports = chaiStyle
+
+function chaiStyle() {
+// function chaiStyle(chai, utils) {
+  // const {Assertion} = chai
+
+  // your helpers here
+}
--- sources/index.spec.js
+++ sources/index.spec.js
@@ -1,9 +1,11 @@
 const {describe, it} = require('mocha')
 const {expect} = require('chai')
-  // .use(require('./index.js'))
+  .use(require('./index.js'))
 
 describe('chai-style', () => {
-  it('fake test', () => {
-    expect(true).to.be.true
+  it('should exports a named function', () => {
+    const module = require('./index.js')
+    expect(module).to.be.a('function')
+    expect(module.name).to.be.equal('chaiStyle')
   })
 })
